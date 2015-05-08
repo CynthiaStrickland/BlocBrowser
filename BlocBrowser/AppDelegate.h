18afe01a56d7e91861d1f43893ec009a5bf500e7
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-07 22:45:07 -0700

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Cynthia Whitlatch on 5/7/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Cynthia Whitlatch on 5/7/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
