b0c8edbbb31a52c37586aa421c3779b19de0a8b5
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-09 21:07:38 -0700

    exercise-23

--- BlocBrowser/AppDelegate.m
+++ BlocBrowser/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "ViewController.h"
+//new
 
 @interface AppDelegate ()
 
--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -5,6 +5,7 @@
 //  Created by Cynthia Whitlatch on 5/7/15.
 //  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
 //
+//new
 
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
