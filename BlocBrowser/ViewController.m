cd3fe8f1dbe5f72866559205f7dc4a54d5a651d7
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-05-10 22:27:02 -0700

    awesome-colorful-toolbar

--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -9,42 +9,204 @@
 
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
+#import "AwsomeFloatingToolbar.h"
 
-@interface ViewController () <WKNavigationDelegate>
+#define kWebBrowserBackString NSLocalizedString(@"Back", @"Back command")
+#define kWebBrowserForwardString NSLocalizedString(@"Forward", @"Forward command")
+#define kWebBrowserStopString NSLocalizedString(@"Stop", @"Stop command")
+#define kWebBrowserRefreshString NSLocalizedString(@"Refresh", @"Reload command")
 
+@interface ViewController () <WKNavigationDelegate, UITextFieldDelegate, AwesomeFloatingToolbarDelegate>
 
 @property (nonatomic, strong) WKWebView *webView;
-//@property(nonatomic, assign) id< UIWebViewDelegate > delegate
+@property (nonatomic, strong) UITextField *textField;
+//@property (nonatomic, strong) UIButton *backButton;
+//@property (nonatomic, strong) UIButton *forwardButton;
+//@property (nonatomic, strong) UIButton *stopButton;
+//@property (nonatomic, strong) UIButton *reloadButton;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) AwesomeFloatingToolbar *awesomeToolbar;
 
 @end
 
 @implementation ViewController
 
+- (void) resetWebView {
+    [self.webView removeFromSuperview];
+    WKWebView *newWebView = [[WKWebView alloc] init];
+    newWebView.navigationDelegate = self;
+    [self.view addSubview:newWebView];
+    
+    self.webView = newWebView;
+    
+    self.textField.text = nil;
+    [self updateButtonsAndTitle];
+}
+
+#pragma mark - UIViewController
+
 - (void)loadView {
     UIView *mainView = [UIView new];
-
+    
     self.webView = [[WKWebView alloc] init];
     self.webView.navigationDelegate = self;
     
-    NSString *urlString = @"http://wikipedia.org";
-    NSURL *url = [NSURL URLWithString:urlString];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:request];
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"Website URL or Search Criteria", @"Placeholder text for web browser URL field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+    
+    self.awesomeToolbar = [[AwesomeFloatingToolbar alloc] initWithFourTitles:@[kWebBrowserBackString, kWebBrowserForwardString, kWebBrowserStopString, kWebBrowserRefreshString]];
+    self.awesomeToolbar.delegate = self;
     
     [mainView addSubview:self.webView];
+    [mainView addSubview:self.textField];
+    
+    for (UIView *viewToAdd in @[self.webView, self.textField, self.awesomeToolbar]) {
+        [mainView addSubview:viewToAdd];
+    }
+    
     self.view = mainView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+    
+    [self.activityIndicator startAnimating];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
-    
+
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-        
-    //make the webview fill the main view
+    
+    //make the webView fill the main view
     self.webView.frame = self.view.frame;
+    
+    // First, calculate some dimensions.
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    
+    //CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+
+    // Now, assign the frames
+    
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+   
+    self.awesomeToolbar.frame = CGRectMake(20, 100, 280, 60);
+}
+
+
+#pragma mark - UITextFieldDelegate
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title {
+    if ([title isEqual:NSLocalizedString(@"Back", @"Back command")]) {
+            [self.webView goBack];
+        } else if ([title isEqual:NSLocalizedString(@"Forward", @"Forward command")]) {
+            [self.webView goForward];
+        } else if ([title isEqual:NSLocalizedString(@"Stop", @"Stop command")]) {
+            [self.webView stopLoading];
+        } else if ([title isEqual:NSLocalizedString(@"Refresh", @"Reload command")]) {
+            [self.webView reload];
+        }
+    }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    NSString *URLString = textField.text;
+    
+    NSURL *URL = [NSURL URLWithString:URLString];
+    
+    if (!URL.scheme) {
+        // The user didn't type http: or https:
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+    // The user typed a space so we assume it's a search engine query *********************
+    // THIS IS WHERE THE ERROR IS OCCURING.  NSRangeException NSCFString replaceCharactersInRanger:withString:   Range or index out of bounds
+    
+    NSRange urlSpace = [URLString rangeOfString:@" "];
+    NSString *urlNoSpace = [URLString stringByReplacingCharactersInRange:urlSpace withString:@"+"];
+    
+    
+    if (urlSpace.location != NSNotFound) {
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.google.com/search?q=%@", urlNoSpace]];
+    }
+    
+    //***************************************
+    if (URL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        [self.webView loadRequest:request];
+    }
+    
+    return NO;
+}
+
+#pragma mark - WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    [self updateButtonsAndTitle];
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [self updateButtonsAndTitle];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error
+{
+    if (error.code != NSURLErrorCancelled) {
+        
+        //DISPLAY WELCOME *****************
+        
+        UIAlertView *welcome = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Welcome!", @"Welcome title")
+                                                          message:NSLocalizedString(@"Get excited to use the best web browser ever!", @"Welcome comment")
+                                                         delegate:nil
+                                                cancelButtonTitle:NSLocalizedString(@"OK, I'm excited!", @"Welcome button title") otherButtonTitles:nil];
+        [welcome show];
+        
+        //******************   Nothing is showing up !
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error", @"Error")
+                                                                       message:[error localizedDescription]
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK", nil)
+                                                           style:UIAlertActionStyleCancel handler:nil];
+        
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    
+    [self updateButtonsAndTitle];
+}
+
+#pragma mark - Miscellaneous
+
+- (void) updateButtonsAndTitle {
+    NSString *webpageTitle = [self.webView.title copy];
+    if ([webpageTitle length]) {
+        self.title = webpageTitle;
+    } else {
+        self.title = self.webView.URL.absoluteString;
+    }
+    
+    [self.awesomeToolbar setEnabled:[self.webView canGoBack] forButtonWithTitle:kWebBrowserBackString];
+    [self.awesomeToolbar setEnabled:[self.webView canGoForward] forButtonWithTitle:kWebBrowserForwardString];
+    [self.awesomeToolbar setEnabled:[self.webView isLoading] forButtonWithTitle:kWebBrowserStopString];
+    [self.awesomeToolbar setEnabled:![self.webView isLoading] && self.webView.URL forButtonWithTitle:kWebBrowserRefreshString];
 }
 
 @end
